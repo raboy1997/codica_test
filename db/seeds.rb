66ad931e7ec491ed1948cdeef9c6fbad5bf85629
raboy1997 <maks.rublya1997>
2018-02-08 14:30:33 +0200

    add some fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
-#User.create!(email: 'book_moderator@example.com', password: 'password', password_confirmation: 'password',
-#               role: 'book_moderator')
-#User.create!(email: 'category_moderator@example.com', password: 'password', password_confirmation: 'password',
-#               role: 'category_moderator')
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
+User.create!(email: 'book_moderator@example.com', password: 'password', password_confirmation: 'password',
+               role: 'book_moderator')
+User.create!(email: 'category_moderator@example.com', password: 'password', password_confirmation: 'password',
+               role: 'category_moderator')
 
 #for test
 
